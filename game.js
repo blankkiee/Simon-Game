ba5e122072efd68d4b64934689167487983a4f7e
MarkDaDave <mdmartin983@gmail.com>
2024-10-24 02:57:58 +0800

    updated

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
 
 var started = false;
+
 var level = 0;
 
 $(document).keypress(function () {
@@ -13,25 +15,44 @@ $(document).keypress(function () {
   }
 });
 
-$(".btn").on("click", function () {
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+
   playSound(userChosenColour);
   animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success!");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
+
 function nextSequence() {
+  userClickedPattern = [];
   level++;
+
   $("#level-title").text("Level " + level);
-  var randonNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randonNumber];
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   $("#" + randomChosenColour)
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-
   playSound(randomChosenColour);
 }
 
@@ -40,50 +61,9 @@ function playSound(name) {
   audio.play();
 }
 
-function animatePress(currentColour) {
-  $("#" + currentColour).addClass("pressed");
-
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
   setTimeout(function () {
-    $("#" + currentColour).removeClass("pressed");
-  }, 300);
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
-
-// var color = ["blue", "green", "red", "yellow"];
-// var pushPattern = [];
-// var userPattern = [];
-
-// function playSound(name) {
-//   var audio = new Audio("sounds/" + name + ".mp3");
-//   audio.play();
-// }
-
-// function animatePress(currentColor) {
-//   $("#" + currentColor).addClass("pressed");
-
-//   setTimeout(function () {
-//     $("#" + currentColor).removeClass("pressed");
-//   }, 100);
-// }
-
-// //Handle clicks
-// $(".btn").on("click", function () {
-//   var userChosenColour = $(this).attr("id");
-//   userPattern.push(userChosenColour);
-//   playSound(userChosenColour);
-//   animatePress(userChosenColour);
-// });
-
-// function nextSequence() {
-//   var randonNumber = Math.floor(Math.random() * 4);
-//   var chosenColour = color[randonNumber];
-
-//   pushPattern.push(chosenColour);
-
-//   $("#" + chosenColour)
-//     .fadeIn(100)
-//     .fadeOut(100)
-//     .fadeIn(100);
-
-//   playSound(chosenColour);
-//   // animatePress(chosenColour);
-// }
